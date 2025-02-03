518a82a3787ac63b6410d7e2474350c2f3d266ea
Shantanu <shantanu22100@iiitnr.edu.in>
2025-02-03 23:55:06 +0530

    uses dotenv package now

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -2,11 +2,11 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.20",
-  networks: {
-    sepolia: {
-      url: process.env.ALCHEMY_SEPOLIA_URL,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
+    solidity: "0.8.20",
+    networks: {
+        sepolia: {
+            url: process.env.SEPOLIA_URL,
+            accounts: [process.env.PRIVATE_KEY]
+        }
+    }
 };
